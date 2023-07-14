99e011cd05f0d5ae0997b2b21c05ef1a5d9b3acd
Rostislav <yf1nf2kb3z@gmail.com>
2023-07-14 13:49:14 +0300

    First commit

---
+++ src/main/java/com/relaxingleg/Main.java
@@ -0,0 +1,26 @@
+package com.relaxingleg;
+
+import com.relaxingleg.commands.music.NowPlaying;
+import com.relaxingleg.commands.music.Play;
+import com.relaxingleg.commands.music.Queue;
+import com.relaxingleg.commands.music.Repeat;
+import com.relaxingleg.commands.music.Skip;
+import com.relaxingleg.commands.music.Stop;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+
+import javax.security.auth.login.LoginException;
+public class Main {
+    public static void main(String[] args) throws LoginException {
+        JDA jda = JDABuilder.createDefault(Token.TOKEN).build();
+        jda.addEventListener(new Listeners());
+        CommandManager manager = new CommandManager();
+        manager.add(new Play());
+        manager.add(new Skip());
+        manager.add(new Stop());
+        manager.add(new NowPlaying());
+        manager.add(new Queue());
+        manager.add(new Repeat());
+        jda.addEventListener(manager);
+    }
+}
